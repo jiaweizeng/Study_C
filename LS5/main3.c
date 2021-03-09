297208b0e890c85b2e55d45b4189077ea691c1fc
jiaweizeng <597800295@qq.com>
2021-03-09 19:34:57 +0800

    添加注释

--- LS5/main3.c
+++ LS5/main3.c
@@ -10,6 +10,10 @@ int main() {
         float score;  //成绩  4
     } person1,person2;
     printf(" person1 %d ", sizeof(float));
+    //64位的数据每次都是8个字节8个字节的存
+    //name  num加起来已经五个字节了   剩余三个字节不够存group 会在第二个八个字节里面存group
+    //以此类推   person1实际大小是24个字节
+    //也说明栈空间存储不是完全连续的    但是数组是完全连续的
     printf(" person1 %d ", sizeof(person1));
     return 0;
 }
