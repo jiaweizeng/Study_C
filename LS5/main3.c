7862a94694e2dcf5421d92a5aa5e43c0716888c3
jiaweizeng <597800295@qq.com>
2021-03-09 20:12:27 +0800

    结构体大小还要遵守最大字节的倍数的规律

--- LS5/main3.c
+++ LS5/main3.c
@@ -14,6 +14,8 @@ int main() {
     //name  num加起来已经五个字节了   剩余三个字节不够存group 会在第二个八个字节里面存group
     //以此类推   person1实际大小是20个字节
     //也说明栈空间存储不是完全连续的    但是数组是完全连续的
+    //还要遵守最大字节的整数倍，比如Person里最大的字节的就是int，所以Person大小肯定是4的倍数
+    //假如Person里面还有个Node结构体，Node结构体里面有double，那此时Person的大小肯定就是8的倍数
     printf(" person1 %d ", sizeof(person1));
     return 0;
 }
