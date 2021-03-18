84fd7afbb0a13df6f9887e18270bf4a680923c12
jiaweizeng <597800295@qq.com>
2021-03-18 19:13:37 +0800

    结构体和文件复制

---
+++ LS6/cmake-build-debug/CMakeFiles/LS6.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LS6.dir/main2.c.o"
+  "LS6.exe"
+  "LS6.pdb"
+  "libLS6.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/LS6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
