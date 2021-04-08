89c763b33871976428f3ed03dd99422fbd2f2314
jiaweizeng <597800295@qq.com>
2021-04-08 20:16:32 +0800

    C++ 03

---
+++ LS9/cmake-build-debug/CMakeFiles/LS9.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LS9.dir/main.cpp.o"
+  "LS9.exe"
+  "LS9.pdb"
+  "libLS9.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LS9.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
