7a0f25182213dfc6a091eede58dc45f52f4087dd
jiaweizeng <597800295@qq.com>
2021-04-13 20:01:08 +0800

    LS10

---
+++ LS10/cmake-build-debug/CMakeFiles/LS10.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LS10.dir/main.cpp.o"
+  "LS10.exe"
+  "LS10.pdb"
+  "libLS10.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LS10.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
